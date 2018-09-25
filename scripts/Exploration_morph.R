bad7833bcaf722f440b160f33ee223cd16dd70e4
sammonsm <sammonsm@kenyon.edu>
2018-09-25 13:02:11 -0400

    Work within exploration

--- scripts/Exploration_morph.R
+++ scripts/Exploration_morph.R
@@ -5,6 +5,7 @@ library(mosaic)
 library(tidyverse)
 library(dplyr)
 library(tidyr)
+library(plyr)
 
 #Color libs
 # Install
@@ -52,13 +53,15 @@ t.test(claw.vol~ Hand, data = morph.data)
 
 
 #ggplot2 goodness
-ggplot(data=morph.data, aes(claw.vol)) + 
-  geom_histogram(bins=6)+
-  xlab("Claw Volume (cm^3)") +
-  ylab("Number of Males")
+#distribution of body masses
+ggplot(data=morph.data, aes(mass)) + 
+  geom_histogram(bins=6, fill="forest green", color= "black")+
+  xlab("Body Mass (g)") +
+  ylab("Number of Males")+
+  ggtitle("Distribution of Body Masses of Male Fiddler Crabs")
 
 
-#plotting dat claw volume against dat boody mass
+#plotting claw volume against body mass
 ggplot()+
   geom_point(data=morph.data, aes(x = log(mass), y = log(claw.vol)), color = "forest green") +
   geom_smooth(data=morph.data, aes(x = log(mass), y = log(claw.vol)), method = "lm", se = FALSE, color= "black")+
@@ -66,9 +69,7 @@ ggplot()+
   ylab("Log of Major Cheliped Volume (cm^3)")
 
 
-
-
-#plotting dat claw area against dat boody mass
+#plotting claw area against body mass
 ggplot()+
   geom_point(data=morph.data, aes(x = log(mass), y = log(claw.area)), color = "forest green") +
   geom_smooth(data=morph.data, aes(x = log(mass), y = log(claw.area)), method = "lm", se = FALSE, color= "black")+
@@ -78,6 +79,7 @@ ggplot()+
 #All the plots?
 #Making a way to look at all morphology measures
 
+#Not great, but does something!
 ggplot(morph.data, aes((log(mass)))) + 
   geom_point(aes(y = log(A.cm)), size = 2, colour = "red") +
   geom_point(aes(y = log(C.cm)), size = 2, colour = "orange")+
@@ -86,25 +88,37 @@ ggplot(morph.data, aes((log(mass)))) +
   geom_point(aes(y = log(claw.vol)), size = 2, colour = "blue")+
   geom_point(aes(y = log(claw.area)), size = 2, colour = "purple")
 
-#Creating a working DF of transformed data
+#Creating a working df of transformed data
 
 #Subsetting the data from morph.data that is needed
 
 #Designating which variables will be subset
 working.morph.data.vars <- c("mass","A.cm","B.cm","C.cm","D.cm","E.cm","F.cm","thickness.cm","claw.area","claw.vol")
 
+working.morph.data.vars2 <- c("Hand","A.cm","B.cm","C.cm","D.cm","E.cm","F.cm","thickness.cm","claw.area","claw.vol")
+
 #Creating a new df with just the variables of "working.morph.data.vars"
 working.morph.data <- morph.data[working.morph.data.vars]
 View(working.morph.data)
 
-#Transforming the new working df using r package "dplyr"/"tidyr"
+#Another one to do bw plots with handidness
+working.morph.data2 <- morph.data[working.morph.data.vars2]
+View(working.morph.data2)
+
+#Transforming the new working df using r package "dplyr"/"tidyr" (not really sure which ¯\_(ツ)_/¯)
 help("gather")
-#THIS FUCKING WORKS!!!!
+#THIS WORKS!!!!
 long.morph.data <- working.morph.data %>% gather(morph.descriptor, morph.value, A.cm:claw.vol)
 View(long.morph.data)
 
+#One with handidness
+long.morph.data2 <- working.morph.data2 %>% gather(morph.descriptor, morph.value, A.cm:claw.vol)
 
-#Creating a labellar
+#Ommitting NAs that I thought were already gone
+new.long.morph.data2 <- na.omit(long.morph.data2)
+
+
+#Creating a labellar (still fiddling with this as of 9/25/18)
 Morph.Names <- c(
   `A.cm` = "Claw Length (cm)",
   `B.cm` = "Dactyl Length (cm)",
@@ -127,20 +141,31 @@ ggplot(long.morph.data, aes(x=log(mass), y=log(morph.value))) +
   ylab("log(morph value)") + 
   ggtitle("Scaling of Body Mass to Morphological Values in Fiddler Crabs")+
   facet_wrap(~morph.descriptor, scales = "free")+
-  geom_smooth(data=long.morph.data, aes(x = log(mass), y = log(morph.value)), method = "lm", se = FALSE, color= "purple")
+  geom_smooth(data=long.morph.data, aes(x = log(mass), y = log(morph.value)), method = "lm", se = TRUE, color= "purple")
   
+#Facetted figure of morph data bw plots with handidness because why not
+ggplot(new.long.morph.data2, aes(x=Hand, y=morph.value, fill=Hand)) +
+  geom_boxplot() +
+  ylab("Morphological Value") + 
+  ggtitle("Handidness Across Morphological Traits")+
+  facet_wrap(~morph.descriptor)+
+  scale_fill_discrete(name="Major Cheliped",
+                      breaks=c("R", "L"),
+                      labels=c("Right-Handed", "Left-Handed"))
 
 #Extracting summary statistics on each model in order to look at equations for lines
 morph.info<-
   long.morph.data %>%
-  group_by(morph.descriptor) %>%
-  summarize( = mean(weight),
-            min_weight = min(weight))
+  subset(morph.descriptor)%>%
+  morph.value.lm <- lm(log(morph.value)~log(mass), data = long.morph.data)%>%
+  summary(morph.value.lm)
+
+morph.info
 
 #The really long way
-xyplot(log(A.cm) ~ log(mass), data = working.morph.data, type=c("p","r")) 
+A.cm.lm <- lm(log(A.cm)~log(mass), data = working.morph.data)
 
 #Stats
-summary(lm(log(claw_area) ~ log(mass), data = morph))
+summary(A.cm.lm)
 
 
